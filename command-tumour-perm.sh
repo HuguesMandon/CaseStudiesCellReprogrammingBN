a4ed2a3bb0f6e831c8b088b2f0d6855128aab51a
Hugues Mandon <hugues.mandon@lri.fr>
2019-09-26 13:52:46 +0200

    adding Jupyter notebooks, Cabean linux and mac, pc12 model, and tumour models with constraints

---
+++ command-tumour-perm.sh
@@ -0,0 +1,8 @@
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_PermConstraints_ApopToEMT.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_PermConstraints_ApopToMigr.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_PermConstraints_HsToEMT.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_PermConstraints_EMTToApop.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_PermConstraints_EMTToHS.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_PermConstraints_MigrToApop.ispl
+
+
---
+++ command-tumour.sh
@@ -0,0 +1,13 @@
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_No_DNAdamage_ECMicroEnv_ApopToEMT.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_No_DNAdamage_ECMicroEnv_ApopToHS.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_No_DNAdamage_ECMicroEnv_ApopToMigr.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_No_DNAdamage_ECMicroEnv_EMTToApop.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_No_DNAdamage_ECMicroEnv_EMTToHS.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_No_DNAdamage_ECMicroEnv_EMTToMigr.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_No_DNAdamage_ECMicroEnv_HsToApop.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_No_DNAdamage_ECMicroEnv_HsToEMT.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_No_DNAdamage_ECMicroEnv_HsToMigr.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_No_DNAdamage_ECMicroEnv_MigrToApop.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_No_DNAdamage_ECMicroEnv_MigrToEMT.ispl
+./mcmas-seq-linux -steadystates -asynbn -newtarjan -newpred -control 2 -path 0 -pc 30 tumour/Tumour_No_DNAdamage_ECMicroEnv_MigrToHS.ispl
+
